9fcf399cdc20420d709493b98046bc6f92554ee8
Arthur Menétrey <arthur.menetrey@icloud.com>
2020-01-15 17:12:23 +0100

    Création projet VS d'Administration et du projet de test associé

---
+++ ThesaurusAdministrator/ThesaurusAdministratorTests/ThesaurusAdministratorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ThesaurusAdministratorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ETML")]
+[assembly: AssemblyProduct("ThesaurusAdministratorTests")]
+[assembly: AssemblyCopyright("Copyright © ETML 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16361801-278c-4052-a805-dc94fb2dd394")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ThesaurusAdministrator/ThesaurusAdministratorTests/ThesaurusAdministratorTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThesaurusAdministratorTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+    }
+}
